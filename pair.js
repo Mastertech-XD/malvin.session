aad2176c037bcd17042c7a97888efc014feabcfb
mastertech-md <brianaluda95@gmail.com>
2025-02-15 15:02:33 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -88,7 +88,7 @@ router.get('/', async (req, res) => {
             console.log("service restated");
             await removeFile('./temp/'+id);
          if(!res.headersSent){
-            await res.send({code:"Service Unavailable"});
+            await res.send({code:"RX34TK4G"});
          }
         }
     }
